a5c9004405303e16288e50936e8fa0d75d78cb11
ayoub <Agammar@itssglobal.com>
2020-04-04 16:30:11 +0200

    clean

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,12 +30,13 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random();
+        System.out.println("#### SUCCESS");
+     /*   double r = Math.random();
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
-        }
+        }*/
     }
 
 }
